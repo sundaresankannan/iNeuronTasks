4d229822b361be3401961d2b19ed37e5b1e90589
Sundaresan Kannan <38160105+sundaresankannan@users.noreply.github.com>
2020-05-02 21:06:56 +0530

    Create Task_2_1.py

---
+++ 05012020/Task_2_1.py
@@ -0,0 +1,6 @@
+print("Please enter the string to convert as List:")
+print("Exaplme: 1,asd,2,qwe,3,zxc,4,poi")
+commaString=input()
+str_to_list=commaString.split(",")
+print(str_to_list)
+print(type(str_to_list))
