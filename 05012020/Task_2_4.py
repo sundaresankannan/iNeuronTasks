c148d146f49142c45dbb40bf99495113003a1214
Sundaresan Kannan <38160105+sundaresankannan@users.noreply.github.com>
2020-05-02 21:08:01 +0530

    Create Task_2_4.py

---
+++ 05012020/Task_2_4.py
@@ -0,0 +1,7 @@
+"""Write a Python Program to print the given string in the format specified in the sample output.
+WE, THE PEOPLE OF INDIA, having solemnly resolved to constitute India into a
+SOVEREIGN, SOCIALIST, SECULAR, DEMOCRATIC REPUBLIC and to secure to all
+its citizens
+"""
+format_str="WE, THE PEOPLE OF INDIA,\n\t having solemnly resolved to constitute India into a SOVEREIGN,\n\t\t SOCIALIST, SECULAR, DEMOCRATIC REPUBLIC\n\t\t and to secure to all its citizens"
+print(format_str)
